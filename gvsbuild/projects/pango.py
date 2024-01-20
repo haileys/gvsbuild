7277dd7e4b6a13af2df529861bf98c5d18e538fe
Hailey Somerville <hailey@hailey.lol>
2024-01-20 12:17:59 +1100

    pango: build static

--- gvsbuild/projects/pango.py
+++ gvsbuild/projects/pango.py
@@ -43,6 +43,7 @@ class Pango(Tarball, Meson):
             enable_gi = "disabled"
 
         self.add_param(f"-Dintrospection={enable_gi}")
+        self.add_param("-Ddefault_library=static")
 
     def build(self):
         Meson.build(self)
