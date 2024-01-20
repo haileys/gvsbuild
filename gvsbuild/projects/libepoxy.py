ef61c35a2c6c191891186224be2d5ae5cf686b2a
Hailey Somerville <hailey@hailey.lol>
2024-01-20 12:44:49 +1100

    libepoxy: add patch to fix inconsistent linkage problem

--- gvsbuild/projects/libepoxy.py
+++ gvsbuild/projects/libepoxy.py
@@ -29,6 +29,7 @@ class Libepoxy(Tarball, Meson):
             archive_filename="libepoxy-{version}.tar.gz",
             hash="a7ced37f4102b745ac86d6a70a9da399cc139ff168ba6b8002b4d8d43c900c15",
             dependencies=["ninja", "meson"],
+            patches=["0001-epoxy-shared.patch"]
         )
 
     def build(self):
